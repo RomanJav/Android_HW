47f2622b1998d4b471cb8206e675e545799be189
RomanJav <tomal.roman94@gmail.com>
2019-02-25 20:15:53 +0300

    Переместил ДЗ1 и ДЗ2 в папки hm1 и hm2 соответственно(homework1-2), переписал переход на новую Активити используя companion object

---
+++ app/src/main/java/by/academy/android_hw_tomal/hw2/Hw2Activity.kt
@@ -0,0 +1,12 @@
+package by.academy.android_hw_tomal.hw2
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import by.academy.android_hw_tomal.R
+
+class Hw2Activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw2)
+    }
+}
