34871cefc99f2866fb10d1f353b8a3ffc85576e7
RomanJav <tomal.roman94@gmail.com>
2019-03-01 20:39:51 +0300

    Создание DZ 3

---
+++ app/src/main/java/by/academy/android_hw_tomal/hw3/Hw3Activity.kt
@@ -0,0 +1,12 @@
+package by.academy.android_hw_tomal.hw3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import by.academy.android_hw_tomal.R
+
+class Hw3Activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw3)
+    }
+}
