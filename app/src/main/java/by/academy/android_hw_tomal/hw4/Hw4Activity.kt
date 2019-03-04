95866fa10d6f933e424cdd812854723d12e32032
Roman <fanat.bulok@gmail.com>
2019-03-04 20:31:37 +0300

    Сделал анимацию Совы по клику

--- app/src/main/java/by/academy/android_hw_tomal/hw4/Hw4Activity.kt
+++ app/src/main/java/by/academy/android_hw_tomal/hw4/Hw4Activity.kt
@@ -1,13 +1,31 @@
 package by.academy.android_hw_tomal.hw4
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
 import by.academy.android_hw_tomal.R
 
 class Hw4Activity: AppCompatActivity() {
+    private lateinit var owlImage : ImageView
+    private lateinit var frameAnimation: AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw4)
+
+        owlImage=findViewById(R.id.owl_image)
+        frameAnimation=owlImage.background as AnimationDrawable
+        owlImage.setOnClickListener{
+            startAnimation()
+        }
+    }
+
+    private fun startAnimation(){
+        frameAnimation.start()
+        if (frameAnimation.isRunning){
+            frameAnimation.stop()
+            frameAnimation.start()
+        }
     }
 }
