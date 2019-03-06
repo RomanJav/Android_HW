7a637f5584bb3c16b9c186409f3713bdb90b3658
Roman <fanat.bulok@gmail.com>
2019-03-06 22:23:08 +0300

    Сделал RadioButtonGroup для удобного переключения между подзаданиями(Сова-Часы-Диаграмма)

--- app/src/main/java/by/academy/android_hw_tomal/hw4/Hw4Activity.kt
+++ app/src/main/java/by/academy/android_hw_tomal/hw4/Hw4Activity.kt
@@ -3,27 +3,46 @@ package by.academy.android_hw_tomal.hw4
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ImageView
+import android.widget.RadioGroup
 import by.academy.android_hw_tomal.R
+import by.academy.android_hw_tomal.hw4.custom_views.Clock
 
-class Hw4Activity: AppCompatActivity() {
-    private lateinit var owlImage : ImageView
+class Hw4Activity : AppCompatActivity() {
+    private lateinit var owlImage: ImageView
     private lateinit var frameAnimation: AnimationDrawable
+    private lateinit var clock: Clock
+    private lateinit var radioViews: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw4)
 
-        owlImage=findViewById(R.id.owl_image)
-        frameAnimation=owlImage.background as AnimationDrawable
-        owlImage.setOnClickListener{
+        owlImage = findViewById(R.id.owl_image)
+        owlImage.visibility = View.INVISIBLE
+        clock = findViewById(R.id.clock)
+        clock.visibility = View.INVISIBLE
+        radioViews = findViewById(R.id.radioViews)
+        radioViews?.setOnCheckedChangeListener { group, checkedId ->
+            if (checkedId.equals(R.id.owl_rb)){
+                owlImage.visibility = View.VISIBLE
+                clock.visibility = View.INVISIBLE
+            }
+            else if (checkedId.equals(R.id.clock_rb)){
+                clock.visibility = View.VISIBLE
+                owlImage.visibility = View.INVISIBLE
+            }
+        }
+        frameAnimation = owlImage.background as AnimationDrawable
+        owlImage.setOnClickListener {
             startAnimation()
         }
     }
 
-    private fun startAnimation(){
+    private fun startAnimation() {
         frameAnimation.start()
-        if (frameAnimation.isRunning){
+        if (frameAnimation.isRunning) {
             frameAnimation.stop()
             frameAnimation.start()
         }
