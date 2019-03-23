4a8074ee713f3f0b88e28a1426325c37220683c6
RomanJav <tomal.roman94@gmail.com>
2019-03-23 10:56:39 +0300

    Готовое ДЗ_7

--- app/src/main/java/by/academy/hwTomal/hw7/Hw7Activity.kt
+++ app/src/main/java/by/academy/hwTomal/hw7/Hw7Activity.kt
@@ -1,58 +1,29 @@
 package by.academy.hwTomal.hw7
 
-import android.content.Context
-import android.content.Intent
+
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import by.academy.android_hw_tomal.R
-import by.academy.hwTomal.hw6.action.CreateStudentActivity
-import by.academy.hwTomal.hw6.adapter.StudentsListAdapter
-import by.academy.hwTomal.hw6.entity.StudentsListData
-import kotlinx.android.synthetic.main.activity_hw6.*
+import by.academy.hwTomal.hw7.entity.StudentsListData
+import by.academy.hwTomal.hw7.fragment.StudentEditFragment
+import by.academy.hwTomal.hw7.fragment.StudentListFragment
+import kotlinx.android.synthetic.main.fragment_edit_student.*
 
-class Hw7Activity : AppCompatActivity() {
-    private val studentAdapter = StudentsListAdapter()
 
-    companion object {
-        private val CREATE_STUDENT = CreateStudentActivity::class.java
-        private fun createIntent(context: Context, classActivity: Class<*>): Intent {
-            return Intent(context, classActivity)
-        }
-    }
+class Hw7Activity : AppCompatActivity(), StudentListFragment.Updater {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hw6)
+        setContentView(R.layout.activity_hw7)
 
-        val linearLayoutManager = LinearLayoutManager(this)
-        studentRecyclerView.setHasFixedSize(true)
-        studentRecyclerView.layoutManager = linearLayoutManager
-        studentRecyclerView.adapter = studentAdapter
-
-        createStudentButton.setOnClickListener {
-            startActivity(
-                createIntent(
-                    baseContext,
-                    CREATE_STUDENT
-                )
-            )
-        }
+        StudentsListData.isEditInActivity = studentEditFragment?.isInLayout ?: false
     }
 
-    override fun onPause() {
-        if (StudentsListData.isChanged) {
-            studentAdapter.notifyDataSetChanged()
-            StudentsListData.isChanged = false
+    override fun update(id: Int) {
+        if(StudentsListData.isEditInActivity){
+            val fragment = supportFragmentManager.findFragmentById(R.id.studentEditFragment) as StudentEditFragment
+            fragment.showEdit(id)
         }
-        super.onPause()
-    }
-
-    override fun onResume() {
-        if (StudentsListData.isChanged) {
-            studentAdapter.notifyDataSetChanged()
-            StudentsListData.isChanged = false
-        }
-        super.onResume()
     }
 }
---
+++ app/src/main/java/by/academy/hwTomal/hw7/fragment/StudentEditFragment.kt
@@ -0,0 +1,67 @@
+package by.academy.hwTomal.hw7.fragment
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import by.academy.android_hw_tomal.R
+import by.academy.hwTomal.hw7.entity.Student
+import by.academy.hwTomal.hw7.entity.StudentsListData
+import kotlinx.android.synthetic.main.activity_edit_student.*
+
+class StudentEditFragment : Fragment() {
+    lateinit var attachActivity: Activity
+    lateinit var student: Student
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.activity_edit_student, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSave.setOnClickListener {
+            StudentsListData.editStudent(
+                Student(
+                    student.id,
+                    editName.text.toString(),
+                    (editAge.text.toString()).toInt(),
+                    editPhoto.text.toString()
+                )
+            )
+            StudentsListData.isChanged = true
+            if (!StudentsListData.isEditInActivity)
+                activity?.onBackPressed()
+            else
+                activity?.recreate()
+        }
+        btnDelete.setOnClickListener {
+            StudentsListData.deleteStudent(student.id)
+            StudentsListData.isChanged = true
+            if (!StudentsListData.isEditInActivity)
+                activity?.onBackPressed()
+            else
+                activity?.recreate()
+        }
+
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        attachActivity = context as Activity
+    }
+
+    fun showEdit(id: Int) {
+        for (student in StudentsListData.studentsList) {
+            if (student.id == id) {
+                this.student = student
+                editPhoto.setText(student.photo)
+                editName.setText(student.fullName)
+                editAge.setText(student.age.toString())
+            }
+        }
+    }
+}
